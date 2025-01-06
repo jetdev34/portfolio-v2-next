460f68d70cb85fc2c328ed35e7dccd6e95bd33a4
genocider34 <alonzoball07@gmail.com>
2025-01-06 20:01:26 +0800

    fix minor error

--- components/Animation.tsx
+++ components/Animation.tsx
@@ -1,17 +1,17 @@
-"use client";
+// "use client";
 
-import { useLottie } from "lottie-react";
-import animationData from "./animationData.json";
+// import { useLottie } from "lottie-react";
+// import animationData from "./animationData.json";
 
-const Animation = () => {
-  const defaultOptions = {
-    animationData: JSON.parse(JSON.stringify(animationData)), // Create a deep copy
-    loop: true,
-  };
+// const Animation = () => {
+//   const defaultOptions = {
+//     animationData: JSON.parse(JSON.stringify(animationData)), // Create a deep copy
+//     loop: true,
+//   };
 
-  const { View } = useLottie(defaultOptions);
+//   const { View } = useLottie(defaultOptions);
 
-  return <div className="w-[300px] md:w-[350px]">{View}</div>;
-};
+//   return <div className="w-[300px] md:w-[350px]">{View}</div>;
+// };
 
-export default Animation;
+// export default Animation;
